8a6a5f209408ef094678883d587338e44e7af2d1
Amielle <144174335+AmielleE@users.noreply.github.com>
2026-03-18 12:31:28 -0600

    split up the files

---
+++ game/settings.py
@@ -0,0 +1,26 @@
+# Game title
+TITLE = "PeaceBreak"
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 40, 40)
+BRIGHT_RED = (255, 70, 70)
+DARK_BLUE = (20, 30, 60)
+GOLD = (255, 215, 0)
+LIGHT_WOOD = (156, 104, 64)
+DARK_WOOD = (92, 55, 30)
+PANEL_BROWN = (70, 40, 20)
+HIGHLIGHT = (255, 230, 120)
+
+# Screen scaling
+SCREEN_SCALE = 0.27
+SCALE = 0.27 # can be removed if same as SCREEN_SCALE
+
+# Menu
+MENU_WIDTH = 240
+
+# Game timers
+TIP_INTERVAL = 8000 # ms between tip messages
+GAME_DURATION = 180000 # 3 minutes in ms
+BONUS_INTERVAL = 3000  # 3 seconds per bonus tick
